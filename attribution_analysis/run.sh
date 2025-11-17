2d22be8c0ac8fd3ffcb64eb2320834b2a06f9fca
thamolwanpo <ploy_mel@hotmail.com>
2025-11-18 07:46:50 +1100

    Change n_steps

--- attribution_analysis/run.sh
+++ attribution_analysis/run.sh
@@ -2,19 +2,19 @@ python analyze_attributions.py \
     --config ../configs/attribution/nrms_bert_finetune.yaml \
     --dataset benchmark \
     --n_samples 40000 \
-    --n_steps 20 \
+    --n_steps 50 \
     --top_k 15
 
 python analyze_attributions.py \
     --config ../configs/attribution/nrms_bert_finetune.yaml \
     --dataset train_clean \
     --n_samples 40000 \
-    --n_steps 20 \
+    --n_steps 50 \
     --top_k 15
 
 python analyze_attributions.py \
     --config ../configs/attribution/nrms_bert_finetune.yaml \
     --dataset train_poisoned \
     --n_samples 40000 \
-    --n_steps 20 \
+    --n_steps 50 \
     --top_k 15
